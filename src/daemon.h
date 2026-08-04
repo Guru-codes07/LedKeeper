547bc43de0278c760407ba5c5d21c56398552157
Guru-codes07 <2020gurup@gmail.com>
2026-08-04 16:27:25 +0530

    completed dameon.h

--- src/daemon.h
+++ src/daemon.h
@@ -16,19 +16,19 @@ typedef struct {
 //daemon_config_set_defaults - populate *config with DAEMON_DEFAULT_POLL_INTERVAL_MS and verbose = 0.
 void daemon_config_set_defaults(daemon_config_t *config);
 
+/*  daemon_setup_signal_handlers - install handlers for SIGINT and
+    SIGTERM that flag a pending shutdown.
+*/
+int daemon_setup_signal_handlers(void);
 
+/* daemon_should_exit - returns nonzero if SIGINT or SIGTERM has been
+   received since daemon_setup_signal_handlers() was called.
+*/
+int daemon_should_exit(void);
 
-
-
-
-
-
-
-
-
-
-
-
-
+/* daemon_run - run the monitor loop: repeatedly check led's
+ brightness, restore it via led_turn_on() whenever it reads as off.
+*/
+int daemon_run(const led_info_t *led, const daemon_config_t *config);
 
 #endif
