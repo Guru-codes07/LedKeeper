98f62d7f1432b85e3ed11effac630e35353efac5
Guru-codes07 <2020gurup@gmail.com>
2026-08-07 17:53:12 +0530

    started work in logger.c

--- src/logger.c
+++ src/logger.c
@@ -10,3 +10,16 @@
 static const char *g_ident = "ledkeeper";
 static int g_use_syslog = 0;
 static int g_verbose = 0;
+
+void logger_init(const char *ident, int use_syslog, int verbose) 
+{
+    g_ident = (ident != NULL) ? ident : "ledkeeper";
+    g_use_syslog = use_syslog;
+    g_verbose = verbose;
+
+    if (g_use_syslog) 
+    {
+        openlog(g_ident, LOG_PID, LOG_DAEMON);
+    }
+}
+
