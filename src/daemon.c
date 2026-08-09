a21573dbb89fcc92da75d6cad2c802b91aac528b
Guru-codes07 <2020gurup@gmail.com>
2026-08-09 18:28:44 +0530

    initialising daemon.c

--- src/daemon.c
+++ src/daemon.c
@@ -9,3 +9,18 @@
 #include <time.h>
 
 static volatile sig_atomic_t g_shutdown_requested = 0;
+static void handle_shutdown_signal(int signum) 
+{
+    (void)signum;
+    g_shutdown_requested = 1;
+}
+
+void daemon_config_set_defaults(daemon_config_t *config) 
+{
+    if (config == NULL) 
+    {
+        return;
+    }
+    config->poll_interval_ms = DAEMON_DEFAULT_POLL_INTERVAL_MS;
+    config->verbose = 0;
+}
