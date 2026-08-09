9a5d65efcbac85e005b98e42eebe11eb18129539
Guru-codes07 <2020gurup@gmail.com>
2026-08-09 18:28:01 +0530

    started work on daemon.c

--- src/daemon.c
+++ src/daemon.c
@@ -1,3 +1,11 @@
 // LEDKEEPER - A C project for controlling keyboard LEDs
 // LEDKEEPER V1.0
+// daemon.c - daemon lifecycle: signal handling and the monitor loop
+#include "daemon.h"
+#include "logger.h"
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+#include <time.h>
 
+static volatile sig_atomic_t g_shutdown_requested = 0;
