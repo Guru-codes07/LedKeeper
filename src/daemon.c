4e35aacc96decfb2932e343960d166bac3630452
Guru-codes07 <2020gurup@gmail.com>
2026-08-14 17:20:44 +0530

    working on daemon.c

--- src/daemon.c
+++ src/daemon.c
@@ -24,3 +24,22 @@ void daemon_config_set_defaults(daemon_config_t *config)
     config->poll_interval_ms = DAEMON_DEFAULT_POLL_INTERVAL_MS;
     config->verbose = 0;
 }
+  int daemon_setup_signal_handlers(void) 
+{
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = handle_shutdown_signal;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    if (sigaction(SIGINT, &sa, NULL) != 0) 
+    {
+     return -1;
+    }
+     if (sigaction(SIGTERM, &sa, NULL) != 0) 
+    {
+     return -1;
+    }
+
+    return 0;
+}
